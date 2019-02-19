b34fbdf48b2a12f79f72a5af7038771a139d8b3d
andychen120 <andychen120@gmail.com>
2019-02-19 18:27:26 +0800

    Add inventory function

--- inventory/handler.go
+++ inventory/handler.go
@@ -2,20 +2,78 @@ package function
 
 import (
 	"database/sql"
-	"fmt"
+	"encoding/json"
+	_ "fmt"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/openfaas-incubator/go-function-sdk"
 )
 
+type Data struct {
+	status   string
+	quantity int
+	count    int
+}
+
+var db *sql.DB
+
+func connectDB() error {
+	var err error
+	db, err = sql.Open("mysql", "myuser:mypass@tcp(34.80.17.17:3306)/pentium")
+	if err != nil {
+		return err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
 // Handle a function invocation
 func Handle(req handler.Request) (handler.Response, error) {
-	var err error
+	message := ""
+	err := connectDB()
+	if err != nil {
+		return handler.Response{
+			Body:       []byte(err.Error()),
+			StatusCode: http.StatusOK,
+		}, err
+	}
+	defer db.Close()
 
-	message := fmt.Sprintf("Hello world, input was: %s", string(req.Body))
+	datas := make([]*Data, 0)
+	rows, _ := db.Query("SELECT status, quantity, COUNT(*) as count FROM pet_order GROUP BY status, quantity")
+	for rows.Next() {
+		data := new(Data)
+
+		rows.Scan(&data.status, &data.quantity, &data.count)
+		datas = append(datas, data)
+	}
+
+	var result map[string]int
+
+	result = make(map[string]int)
+
+	for _, data := range datas {
+		result[data.status] = result[data.status] + (data.quantity * data.count)
+	}
+
+	jresult, err := json.Marshal(result)
+	if err != nil {
+		message = err.Error()
+	} else {
+		message = string(jresult)
+	}
 
 	return handler.Response{
 		Body:       []byte(message),
 		StatusCode: http.StatusOK,
+		Header: map[string][]string{
+			"Content-Type": {"application/json"},
+		},
 	}, err
 }
